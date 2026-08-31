73ce69e938d78df8de127a19f13143a48cb05fa9
Gabriel Penalber <gabrieloliveirapenalber@gmail.com>
2026-08-31 16:35:10 -0300

    Adicionado função de modo e resolvimento de bugs (ainda tem mais)

--- Penalber/function.py
+++ Penalber/function.py
@@ -9,13 +9,49 @@ FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 FaceLandmarksConnections = mp.tasks.vision.FaceLandmarksConnections
 
-def two_viewports(frame, image, black_image):
+def two_viewports(frame, image, black_image, screen_h, screen_w):
 
-    height = frame.shape[1]
-    width = frame.shape[0]
-    black_image_resized = cv2.resize(black_image, (height, width))
-    image_resized = cv2.resize(image, (height, width))
+    ## Formaliza como vai ficar a proporção da tela.
+    ## Depois da resize nelas para poder caber certinho.
+    ## E por fim junta elas pelo hconcat.
+    h, w = image.shape[:2]
+    ## Mesma coisa do codigo de baixo, usa somente os dois primeiros numeros [altura, largural, canal(ñ usa)]
+
+    double_height = w * 2
+    ## Valor ajustado para a tela dupla ficar certinha, na metada do meu note, vê depois se ficou bom ai.
+
+    
+    scale = min(screen_w / double_height, screen_h / h)
+    height = int(h * scale)
+    width = int(w * scale)
+    black_image_resized = cv2.resize(black_image, (width, height), interpolation = cv2.INTER_AREA)
+    image_resized = cv2.resize(image, (width, height), interpolation = cv2.INTER_AREA)
     final_image = cv2.hconcat([image_resized, black_image_resized])
 
     return final_image
-    
+
+def float_viewport(image, background, scale = 0.2):
+    ## Função recebendo, image = imagem crua da webcam; background = imagem preta sem nenhuma impressão; scale = escala para multiplicar por 0.3.
+
+    bg_h, bg_w = background.shape[:2]
+    ## da nome às variáveis da height e width e marca elas para utilizarem somente 2 valores. [altura, largura, canal (ñ usa esse)]
+
+    float_w = int(bg_w * scale)
+    float_h = int(bg_h * scale)
+    ## Calcula o valor inteiro de pixels pra proporção da tela.
+    floating_resized = cv2.resize(image, (float_w, float_h), interpolation = cv2.INTER_AREA)
+
+    pad = 20
+    y1 = bg_h - float_h - pad
+    y2 = bg_h - pad
+    x1 = bg_w - float_w - pad
+    x2 = bg_w - pad
+
+    ## Calcula a posição de onde a imagem flutuante deve ficar, nesse caso, no canto inferior direito.
+
+    result = background.copy()
+    result[y1:y2, x1:x2] = floating_resized
+    ## Bota a imagemzinha onde ela deve ficar.
+
+
+    return result
--- Penalber/main.py
+++ Penalber/main.py
@@ -2,7 +2,9 @@ import cv2
 import time
 import mediapipe as mp
 import numpy as np
+import tkinter as tk
 from function import two_viewports
+from function import float_viewport
 
 BaseOptions = mp.tasks.BaseOptions
 FaceLandmarker = mp.tasks.vision.FaceLandmarker
@@ -13,7 +15,7 @@ FaceLandmarksConnections = mp.tasks.vision.FaceLandmarksConnections
 options = FaceLandmarkerOptions(
     base_options=BaseOptions(model_asset_path='face_landmarker.task'),
     running_mode=VisionRunningMode.VIDEO,
-    num_faces=1,
+    num_faces=5,
     min_face_detection_confidence=0.5,
     min_tracking_confidence=0.5
 )
@@ -22,26 +24,41 @@ webcam = cv2.VideoCapture(0)
 webcam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
+## Fiz isso pra webcam capturar com qualidade.
+
+root = tk.Tk()
+root.withdraw()
+screen_w = root.winfo_screenwidth()
+screen_h = root.winfo_screenheight()
+root.destroy()
+
+## ele abre uma janelinha baseada no tkinker para verificar o tamanho da tela do usuario e depois pega os dados e destroi essa telinha.
+
+screen_mode = 1
+
+
 with FaceLandmarker.create_from_options(options) as landmarker:
     while True:
         valid, frame = webcam.read()
 
         timestamp_ms = int(time.time() * 1000)
-    
+    ## Criei os frames que estarão na tela.
         frame = cv2.flip(frame, 1)
-        frame = cv2.resize(frame, (960, 540))
         h, w, _ = frame.shape
-
+    ## Criei a variavel da altura e largura para poder passar a tela preta.
         image = frame.copy()
 
         black_image = np.zeros((h, w, 3), dtype = np.uint8)
+        black_image2 = np.zeros((h, w, 3), dtype = np.uint8)
+    ## Isso aqui faz a tela ficar totalmente preta.
 
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
 
         result = landmarker.detect_for_video(mp_image, timestamp_ms)    
+    ## Faz tudo num unico if
     
-        if result.face_landmarks:
+        if result.face_landmarks:   
             h, w, _ = image.shape
             for face_landmarks in result.face_landmarks:
                     
@@ -62,12 +79,26 @@ with FaceLandmarker.create_from_options(options) as landmarker:
                     cv2.circle(black_image, (cx, cy), 1, (0, 255, 0), -1)
                     cv2.circle(image, (cx, cy), 1, (0, 255, 0), -1)
                        
-        final_image = two_viewports(frame, image, black_image)
+        if  screen_mode == 1:
+            final_image = two_viewports(frame, image, black_image, screen_w, screen_h)
+        else:
+            final_image = float_viewport(image, background = black_image2, scale = 0.2)
+            final_image = cv2.resize(final_image, (1920, 1080))
+
+        ## Roda  as duas funções para criação da segunda tela e já adiciona a tela base com as duas viewports.
 
         cv2.imshow("webcam", final_image)
+        key = cv2.waitKey(5) & 0xFF
 
-        if cv2.waitKey(5) == 27:  # Tecla ESC para fechar
+
+        if key == 27:  # Tecla ESC para fechar
             break
+        elif key == ord('m') or key == ord('M'):
+            screen_mode = 2 if screen_mode == 1 else 1
+
+        ## Usa a  tecla M para mudar a interface.
 
 webcam.release()
 cv2.destroyAllWindows()
+
+## Essa bosta ta descalibrada/glitchando, ja tentei resolver mas n sei como
